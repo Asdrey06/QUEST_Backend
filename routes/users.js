c6f45757016213292a4ba60a0edbbc0b1959d9af
Alexandre <reineart123@gmail.com>
2023-10-23 11:34:31 +0200

    Add routes SignIn and TDD

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
+const User = require('../models/user');
+const { checkBody } = require('../modules/checkBody');
+const bcrypt = require('bcrypt');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+// Function to validate email
+function validateEmail(email) {
+  const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+  return emailRegex.test(email);
+}
+
+// The route allows me to connect to my customer account
+router.post('/signin', (req, res) => {
+  if (!checkBody(req.body, ['email', 'password'])) {
+    res.json({ result: false, error: 'Missing or empty fields' });
+    return;
+  }
+  const { email, password } = req.body;
+  if (!validateEmail(email)) {
+    res.json({ result: false, error: 'Invalid email address' });
+    return;
+  }
+  User.findOne({ email: email }).then(data => {
+    if (data && bcrypt.compareSync(password, data.password)) {
+      res.json({ result: true, token: data.token, data: data });
+    } else {
+      res.json({ result: false, error: 'User not found or wrong password' });
+    }
+  });
 });
 
 module.exports = router;
+
---
+++ users.test.js
@@ -0,0 +1,47 @@
+const request = require('supertest');
+const app = require('./app');
+  
+//Utilisateur inexistant
+  it('POST /signin - Utilisateur inexistant ', async () => {
+    const res = await request(app).post('/users/signin').send({
+        email: 'test@test.com',
+        password: 'mot de passe incorrect',
+      });
+  
+    expect(res.statusCode).toBe(200);
+    expect(res.body.result).toBe(false);
+    expect(res.body.error).toBe('User not found or wrong password');
+  });
+  
+  //Email invalide
+  it('POST /signin - Email invalide', async () => {
+    const res = await request(app).post('/users/signin').send({
+        email: 'email_invalide',
+        password: 'mot de passe incorrect',
+      });
+  
+    expect(res.statusCode).toBe(200);
+    expect(res.body.result).toBe(false);
+    expect(res.body.error).toBe('Invalid email address');
+  });
+  
+  //Champs manquants
+  it('POST /signin - Champs manquants', async () => {
+    const res = await request(app).post('/users/signin').send({});
+    
+    expect(res.statusCode).toBe(200);
+    expect(res.body.result).toBe(false);
+    expect(res.body.error).toBe('Missing or empty fields');
+  });
+
+  //Email valide mais mot de passe incorrect
+  // it('POST /signin - Email valide mais mot incorrect', async () => {
+  //   const res = await request(app).post('/users/signin').send({
+  //    email: 'test@test.com',
+  //    password: 'mot de passe incorrect',
+  //   });
+
+  //   expect(res.statusCode).toBe(200);
+  //   expect(res.body.result).toBe(false)
+  //   expect(res.body.error).toBe('Invalid password')
+  // });
