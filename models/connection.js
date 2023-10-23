ace0d21f587ca04f8ac70fd31455df8c3046eabd
Alexandre <reineart123@gmail.com>
2023-10-23 09:49:00 +0200

    Add connection database backend

---
+++ models/connection.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+
+const connectionString = "mongodb+srv://admin:MstD45atRUPQEBVd@cluster0.btn4jdj.mongodb.net/quest"
+
+mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
+  .then(() => console.log('Database connected'))
+  .catch(error => console.error(error));
