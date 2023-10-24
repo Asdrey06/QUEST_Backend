1cc3d3016c048e9815e0aa019e82817a7326819b
Nikita <kofman.niki@gmail.com>
2023-10-24 17:48:14 +0200

    fixed chat router

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var conciergesRouter = require("./routes/concierges");
 var requestRouter = require("./routes/request");
+var chatRouter = require("./routes/chat");
 
 var app = express();
 
@@ -26,5 +27,6 @@ app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/concierges", conciergesRouter);
 app.use("/request", requestRouter);
+app.use("/chat", chatRouter);
 
 module.exports = app;
