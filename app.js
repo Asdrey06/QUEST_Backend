6bf2cd2805fa3ce23aef67c90f355705614ba800
Nikita <kofman.niki@gmail.com>
2023-10-26 17:46:28 +0200

    fixing backends

--- app.js
+++ app.js
@@ -12,9 +12,10 @@ var conciergesRouter = require("./routes/concierges");
 var requestRouter = require("./routes/request");
 var chatRouter = require("./routes/chat");
 
+const cors = require("cors");
+
 var app = express();
 
-const cors = require("cors");
 app.use(cors());
 
 app.use(logger("dev"));
@@ -23,10 +24,56 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+// Import the Stripe library and set your secret key
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+app.post("/create-payment-intent", async (req, res) => {
+  try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: 1000,
+      currency: "usd",
+    });
+
+    res.json({ clientSecret: paymentIntent.client_secret });
+    console.log(paymentIntent.client_secret);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+    console.log(error);
+  }
+});
+
+app.post("/processpayment", async (req, res) => {
+  try {
+    const { paymentMethodId } = req.body;
+
+    console.log(paymentMethodId);
+
+    const paymentIntent = await stripe.paymentIntents.create({
+      payment_method: paymentMethodId,
+      amount: 1000,
+      currency: "usd",
+      confirm: true,
+      return_url: "https://google.com",
+    });
+    console.log("great payment success");
+
+    return res.json({ success: true, paymentIntent });
+  } catch (error) {
+    console.error("Error processing payment:", error);
+    console.log(error.message);
+    return res.status(500).json({ error: error.message });
+  }
+});
+
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/concierges", conciergesRouter);
 app.use("/request", requestRouter);
 app.use("/chat", chatRouter);
+
+// const port = process.env.PORT || 3000;
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
 
 module.exports = app;
