cc5096a202b0f360a37b4329e3fb3f213207613b
Nikita <kofman.niki@gmail.com>
2023-11-02 09:56:22 +0100

    cors add

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ var app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3001",
+    origin: "https://quest-frontend-chi.vercel.app",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   })
 );
